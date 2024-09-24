de3fa36e4632796150908aa567b56f3808bbf152
Hubert1225 <hubibaran@gmail.com>
2024-09-24 13:43:25 +0200

    add image_utils.py module with rgb2gray function

---
+++ part-3-image-processing/image_utils.py
@@ -0,0 +1,22 @@
+import numpy as np
+import numpy.typing as npt
+
+
+def rgb2gray(rgb: npt.NDArray[np.uint8]) -> npt.NDArray[np.uint8]:
+    """Converts an RGB image to grayscale
+
+    Function adapted from
+    https://www.askpython.com/python-modules/matplotlib/convert-rgb-to-grayscale
+
+    Parameters
+    ----------
+    rgb : 3D array
+        RGB image channels
+
+    Returns
+    -------
+    2D array
+        image in grayscale
+
+    """
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.144])
